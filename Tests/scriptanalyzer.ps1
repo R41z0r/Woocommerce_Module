17f4a37e84444f1811fd1c505de853d1c5b64b8b
Jenkins <philipp.barth@cloudra.de>
2018-06-27 15:11:34 +0200

    'Tests/scriptanalyzer.ps1' hinzufügen

---
+++ Tests/scriptanalyzer.ps1
@@ -0,0 +1,13 @@
+$Results = Invoke-ScriptAnalyzer -Path "$PSScriptRoot\.." -Recurse -Severity Warning,Error -ErrorAction SilentlyContinue
+If ($Results) {
+$ResultString = $Results | Out-String
+Write-Warning $ResultString
+Write-Output "PSScriptAnalyzer output contained one or more result(s) with 'Error' severity Check the 'Tests' tab of this build for more details."
+Exit 1
+
+# Failing the build
+Throw "Build failed"
+}
+Else {
+Exit 0
+}
