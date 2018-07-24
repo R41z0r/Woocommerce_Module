05ef2e854f390a55645aa3a8f490889002600885
R41Z0R <Philipp.Barth@cloudra.de>
2018-07-24 13:30:31 +0200

    Added Testmodes for Get-WooCommerceProduct

--- Tests/Products/Products.Tests.ps1
+++ Tests/Products/Products.Tests.ps1
@@ -110,14 +110,92 @@ Describe "New-WooCommerceProduct" {
 
 Describe "Get-WooCommerceProduct"  {
     
-    Context "Testfile" { 
+    Context "List Product" { 
 
         Set-StrictMode -Version latest
 
-        It "Should list all files in systemdrive" {
-            (Get-ChildItem -Path $env:systemdrive).Count | Should -BeGreaterOrEqual 0
-        }
-    }
+        It "Should list one specific product" {
+			$product = Get-WooCommerceProduct -id $script:wooCommerceProductsArray[0]
+			$product | Should -Not -BeNullOrEmpty
+			$product | Should -HaveCount 1
+			$product.id | Should -EQ $script:wooCommerceProductsArray[0]
+		}
+		
+		It "Should list all products"
+		{
+			$products = Get-WooCommerceProduct -all
+			$products | Should -Not -BeNullOrEmpty
+			($products | Measure-Object).count | Should -HaveCount -BeGreaterThan $script:wooCommerceProductsArray.count
+			$script:wooCommerceProductsArray | Should -BeIn $products.id
+		}
+	}
+}
+
+Describe "Set-WooCommerceProduct"  {
+	
+	Context "New simple Product" {
+		Set-StrictMode -Version latest
+		
+		It "Should create a new simple product" {
+			$newProduct = New-WooCommerceProduct -name "TestPester"
+			$newProduct | Should -Not -BeNullOrEmpty
+			$script:wooCommerceProductsArray += $newProduct.ID
+		}
+		
+		It "Should create a new simple product with price" {
+			$price = 10.01
+			$newProduct = New-WooCommerceProduct -name "TestPester" -regular_price $price
+			$newProduct | Should -not -BeNullOrEmpty
+			$newProduct | Should -HaveCount 1
+			$script:wooCommerceProductsArray += $newProduct.ID
+			$newProduct.regular_price | Should -BeExactly $price
+		}
+		
+		It "Should create a new simple product with all attributes available" {
+			$hashFalseTrue = @{
+				"true"  = $true
+				"false" = $false
+			}
+			$name = "TestPesterAllAttributes"
+			$type = "simple"
+			$description = "ProductTest"
+			$shortDescription = "ShortProductTest"
+			$status = "draft"
+			$slug = "test"
+			$featured = "$([System.String]([boolean](Get-Random 0, 1)))"
+			$catalog_visibility = "hidden"
+			$price = 0.12
+			$sale_price = 0.05
+			$date_on_sale_from = "$(Get-Date -Date (Get-Date).AddDays(1) -Format s)"
+			$date_on_sale_to = "$(Get-Date -Date (Get-Date).AddDays(3) -Format s)"
+			$virtual = "$([System.String]([boolean](Get-Random 0, 1)))"
+			$downloadable = "$([System.String]([boolean](Get-Random 0, 1)))"
+			
+			$newProduct = New-WooCommerceProduct -name $name -type $type `
+												 -description $description -short_description $shortDescription -status $status `
+												 -slug $slug -featured $featured -catalog_visibility $catalog_visibility -regular_price $price `
+												 -sale_price $sale_price -date_on_sale_from $date_on_sale_from -date_on_sale_to $date_on_sale_to `
+												 -virtual $virtual -downloadable $downloadable
+			
+			$newProduct | Should -not -BeNullOrEmpty
+			$newProduct | Should -HaveCount 1
+			$script:wooCommerceProductsArray += $newProduct.ID
+			$newProduct.name | Should -BeExactly $name
+			$newProduct.type | Should -BeExactly $type
+			$newProduct.description | Should -BeExactly $description
+			$newProduct.short_description | Should -BeExactly $shortDescription
+			$newProduct.status | Should -BeExactly $status
+			$newProduct.slug | Should -BeExactly $slug
+			$newProduct.featured | Should -BeExactly $hashFalseTrue["$featured"]
+			$newProduct.catalog_visibility | Should -BeExactly $catalog_visibility
+			$newProduct.regular_price | Should -BeExactly $price
+			$newProduct.sale_price | Should -BeExactly $sale_price
+			$newProduct.date_on_sale_from | Should -BeExactly $date_on_sale_from
+			$newProduct.date_on_sale_to | Should -BeExactly $date_on_sale_to
+			$newProduct.virtual | Should -BeExactly $virtual
+			$newProduct.downloadable | Should -BeExactly $downloadable
+		}
+	}
 }
 
 Describe "Remove-WooCommerceProduct" {
